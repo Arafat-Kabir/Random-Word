a3a6634b07652e5407da83750dc80a5034f4875a
Arafat-Kabir <arafat.sun@gmail.com>
2017-09-13 23:20:03 +0600

    Version 1.0 build complete

--- random-word.py
+++ random-word.py
@@ -1,4 +1,5 @@
 import os
+from random import randint
 
 def clean(text):
     """Removes the comments and redundant spaces and returns a list of lines"""
@@ -37,7 +38,7 @@ def loadDictNames():
     return lst
 
 
-def selecDict(dictNames):
+def selectDict(dictNames):
     print "Available Dictionaries:\n\t|"
     i = 1
     for d in dictNames:
@@ -49,41 +50,89 @@ def selecDict(dictNames):
         if(dselect<len(dictNames)):
             return dictNames[dselect]
         else:
-            close("Invalid input")
+            close(" Invalid input")
     else:
-        close("Invalid input")
+        close(" Invalid input")
 
 
-helpStr = """Following commands are Available:
-    help: prints this help message
-    next: show a new random word
-    hint: shows a sentences using the current word
-    show: reveals the meaning
-    exit: exit the program
-"""
+def currentWord():
+    if curword != '':
+        print ' Current Word:',curword
+    else:
+        print " No word selected yet"
 
+
+def nextWord():
+    global curword,means,sents
+    wleft = len(wordList)
+    if wleft>0:
+        indx = randint(0,wleft-1)
+        txt = open(dictname+'\\'+wordList[indx]).read()
+        data = clean(txt)
+        curword = extract(data,'w')
+        means = extract(data,'m').split(';')
+        sents = extract(data,'s').split('.')
+        del wordList[indx]
+        print " What do you understand by '"+curword+"'"
+    else:
+        close(" No more words left")
+
+
+def showHint():
+    if curword=='':
+        print " No word selected yet"
+        return
+    if len(sents)>0:
+        print ' Hint:',sents.pop().strip()
+    else:
+        print " No more hints left"
+
+
+def showMeaning():
+    if curword=='':
+        print "No word selected yet"
+        return    
+    for m in means:
+        print '',m.strip()
+
+
+def printHelp():
+    print """Following commands are Available:
+    help:   prints this help message
+    next:   show a new random word
+    hint:   shows a sentences using the current word
+    reveal: reveals the meanings
+    this:   shows current word
+    clear:  Clear the screen
+    exit:   exit the program"""
+
+def clearScreen():
+    os.system('cls')
 
 #------ Main Program ------
 os.system('cls')
 dictList = loadDictNames()
-dictname = selecDict(dictList)
+dictname = selectDict(dictList)
+cmdList = {'help':printHelp, 'next':nextWord, 'hint':showHint,'reveal':showMeaning, 'this':currentWord, 'clear':clearScreen, 'exit':exit}
+
 print "Selected dictionary:",dictname
 
 wordList = os.listdir(os.getcwd()+'\\'+dictname)
+curword = ''
+means = []
+sents = []
 
+print ''
+printHelp()
+while True:
+    inp = raw_input("\ncommand$> ")
+    inp = inp.strip().lower()
+    if(inp==''):
+        continue
+    if cmdList.has_key(inp):
+        print ""
+        cmdList.get(inp)()
+    else:
+        print " Invalid Command"
 
-# txt = open('./barron333/zealot.txt').read()
-# data = clean(txt)
-# word = extract(data,'w')
-# means = extract(data,'m').split(';')
-# sents = extract(data,'s').split('.')
-
-
-# # print the elements
-# for l in data:
-#     print l
-
-# print 'word:',word
-# print 'meanings:',means
-# print 'sentences:',sents
 
