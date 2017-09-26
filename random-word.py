6103a123dcdab40716a3506f2a4dbb61e5db327f
Arafat-Kabir <arafat.sun@gmail.com>
2017-09-26 15:27:43 +0600

    'relate' functionality added

--- random-word.py
+++ random-word.py
@@ -1,6 +1,7 @@
 import os
 from random import randint
 from shutil import copyfile
+from glob import glob
 
 
 def clean(text):
@@ -67,9 +68,11 @@ def selectDict(dictNames):
 
 def currentWord():
     if curword != '':
-        print ' Current Word:',curword+'\n'
+        print ' Current Word:',curword
+        print ' Current dict:',dictname
     else:
         print " No word selected yet\n"
+    print ''
 
 
 
@@ -128,7 +131,7 @@ def selectWord():
 
 def showRemaining():
     global inp
-    print ' Remaining words in the dictionary are:\n'
+    print ' Remaining words in the dictionary are:'
     cols = 4  #default number of columns
     # read the second argument as the number of columns
     if len(inp)>1:
@@ -153,6 +156,56 @@ def showRemaining():
             print ''
             i=0
     print '\n\n Total remaining:',len(wordList),'words\n'
+
+
+
+def relateFile():
+    global inp;
+    cols = 4  #default no of columns
+    #process the arguments
+    for i in range(len(inp)-1,0,-1):
+        inp[i] = inp[i].strip()
+        if inp[i] == '':
+            #print 'deleted',i
+            del inp[i]
+        elif inp[i].startswith('-c'):   #set the column number
+            #print 'starts with -c'
+            inp[i] = inp[i][2:]  #srip off the -c 
+            if inp[i].isdigit():
+                #print 'is digit'
+                inp[i] = int(inp[i])
+                if inp[i]<=10 and inp[i]>0:
+                    #print 'assigned cols =', inp[1]
+                    cols = inp[i]
+                    del inp[i]
+    #---check, search and print the matches---
+    if len(inp)<2:
+        print " Enter keywords to search\n"
+    else:
+        fileList = glob(dictname+'\\*')   #load files list
+        searchResult = {}
+        #search through the files
+        for f in fileList:
+            content = open(f).read().lower()
+            for keyword in inp[1:]:
+                if searchResult.has_key(keyword) == False:
+                    searchResult[keyword] = []  #add an empty list
+                if keyword.lower() in content:
+                    temp = len(dictname)+1
+                    temp = f[temp:-4]     #extract the file name only
+                    searchResult[keyword].append(temp)
+        #---print the search result---
+        #print searchResult
+        for key in searchResult:
+            print " Related to",key.upper()+': ',str(len(searchResult[key])),'results'
+            i = 0
+            for r in searchResult[key]:
+                print '   %-15s' %r,
+                i += 1
+                if i==cols:
+                    i = 0
+                    print ''
+            print '\n'
 
 
 
@@ -218,11 +271,12 @@ def printHelp():
     next:      select a new word randomly
     hint:      shows a sentences using the current word
     reveal:    reveals the meanings of current word
-    this:      shows current word
+    this:      shows select word and dictionary name
     mark-hard: Copies the current word into the 'hard' dictionary
     not-hard:  Removes the current word from the 'hard' dictionary
     clear:     Clear the screen
     select:    Select a word by typing it
+    relate:    relates specified keywords with the dictionary words
     remaining: Shows the remaining words in the dictionary
     exit:      exit the program
     """
@@ -233,12 +287,11 @@ def clearScreen():
     os.system('cls')
 
 
-
 #------ Main Program ------
 os.system('cls')
-dictList = loadDictNames()
-dictname = selectDict(dictList)
-cmdList = {'help':printHelp, 'next':nextWord, 'hint':showHint,'reveal':showMeaning, 'this':currentWord, 'mark-hard':markHard, 'not-hard':unmarkHard, 'clear':clearScreen,'remaining':showRemaining, 'select':selectWord, 'exit':exit}
+dictList = loadDictNames()        #loads available dictionary names for selection prompt
+dictname = selectDict(dictList)   #selects and specify the selected dictionary
+cmdList = {'help':printHelp, 'next':nextWord, 'hint':showHint,'reveal':showMeaning, 'this':currentWord, 'mark-hard':markHard, 'not-hard':unmarkHard, 'clear':clearScreen,'remaining':showRemaining, 'select':selectWord,'relate':relateFile, 'exit':exit}
 
 print "Selected dictionary:",dictname
 
